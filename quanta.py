7d40188cb6e8c76802ef0b6fc43352e619a2bb7b
Max Kaye <m@xk.io>
2014-08-23 17:27:45 +1000

    class-ify and tidy

--- quanta.py
+++ quanta.py
@@ -6,6 +6,7 @@ from spore import Spore
 from encodium import *
 
 
+
 # Constants
 
 PUSHBLOCKS = 'pushblocks'
@@ -31,12 +32,115 @@ class State:
 
 # Graph Variables
 
-head = None  # for a block to be added (and become head), parent must be head and uncle must be in orphans
-root = None
-orphans = set()  # for a block to be added (that's not head), parent and uncle must be in orphans
-state = State()
-block_index = {}
+class Graph:
+    def __init__(self):
+        self.head = None  # for a block to be added (and become head), parent must be head and uncle must be in orphans
+        self.root = None
+        self.orphans = set()  # for a block to be added (that's not head), parent and uncle must be in orphans
+        self.state = State()
+        self.block_index = {}
 
+    def set_root(self, root):
+        if self.head == None: self.head = root
+        self.root = root
+        self._add_block(self.root, is_root=True)
+        self.apply_to_state(self.root)
+
+    def _back_up_state(self):
+        self.backup_state = deepcopy(self.state)
+
+    def _restore_backed_up_state(self):
+        self.state = self.backup_state
+
+    def have_block(self, block_hash):
+        return block_hash in self.block_index
+
+    def add_blocks(self, blocks):
+        self._back_up_state()
+        try:
+            for block in blocks: self._add_block(block)
+        except Exception as e:
+            raise e
+            self._restore_backed_up_state()
+
+    def _add_block(self, block, is_root=False):
+        if not is_root:
+            if self.have_block(block.hash):
+                return 'Already have block'
+            if not self.have_block(block.parent_hash) or (block.uncle_hash is not None and not self.have_block(block.uncle_hash)):
+                return 'Absent parent / uncle'
+            block.set_parent(self.block_index[block.parent_hash])
+            if block.uncle_hash is not None: block.set_uncle(block_index[block.uncle_hash])
+            if self.better_than_head(block):
+                self.reorganize_to(block)
+            else:
+                self.orphans.add(block)
+        self.block_index[block.hash] = block
+
+    def reorganize_to(self, block):
+        print('reorg to', block.hash, 'from', self.head.hash)
+        pivot = self.find_pivot(self.head, block)
+        self.mass_unapply(Graph.order_from(pivot, self.head)[1:])
+        self.mass_apply(Graph.order_from(pivot, block)[1:])
+        self.head = block
+
+    # Coin & State methods
+
+    def valid_for_state(self, block):
+        if block.tx == None: return True
+        return self.state.get(block.tx.signature.pub_x) >= block.tx.value
+
+    def apply_to_state(self, block):
+        self.modify_state(block, 1)
+
+    def unapply_to_state(self, block):
+        self.modify_state(block, -1)
+
+    def modify_state(self, block, direction):
+        assert direction in [-1, 1]
+        if block.tx is not None:
+            self.state.move_coins(block.tx.recipient, direction * block.tx.value)
+            self.state.move_coins(block.tx.signature.pub_x, -1 * direction * block.tx.value)
+        self.state.move_coins(block.coinbase, direction * block.coins_generated)
+
+    def mass_unapply(self, path):
+        for block in path[::-1]:
+            self.unapply_to_state(block)
+            self.orphans.add(block)
+
+    def mass_apply(self, path):
+        for block in path:
+            assert self.valid_for_state(block)
+            self.apply_to_state(block)
+            if block in self.orphans: self.orphans.remove(block)
+
+    def better_than_head(self, block):
+        return block.sigmadiff > self.head.sigmadiff
+
+    # Static Methods
+
+    @staticmethod
+    def order_from(early_node, late_node, carry=None):
+        if not carry: carry = []
+        if early_node == late_node:
+            return [late_node]
+        if late_node.parent_hash == 0:
+            raise Exception('Root block encountered unexpectedly while ordering graph')
+        order = exclude_from(Graph.order_from(early_node, late_node.parent), carry)
+        if late_node.uncle is not None:
+            order += exclude_from(Graph.order_from(early_node, late_node.uncle), carry + order)
+        return order + [late_node]
+
+    @staticmethod
+    def find_pivot(b1, b2):
+        # conjecture: rewinding back to the lowest common parent in the primary chain (chain made of pri parents) is sufficient
+        for past_block in b1.primary_chain:
+            if past_block in b2.primary_chain:
+                return past_block
+        return None  # should probably not return None but do something else useful
+
+
+graph = Graph()
 
 # P2P
 
@@ -89,25 +193,32 @@ class QuantaBlock(Encodium):
     timestamp = Integer.Definition()
     nonce = Integer.Definition()
 
+    def __hash__(self):
+        return self.hash
+
     def check(s, changed_attributes):
         assert False not in map(is_32_bytes, [s.parent_hash, s.target, s.coinbase])
         if s.uncle_hash != None: assert is_32_bytes(s.uncle_hash)
         assert False not in map(is_4_bytes, [s.timestamp, s.nonce])
         assert s.target < (DIFF_ONE // (256 ** 3))
-        assert coins_generated(s) >= 0
-        assert s.hash < s.target
+        assert s.coins_generated >= 0
+
+        s.init()
 
     # Setup
 
     def init(self):
         self._sigmadiff = None
         self._primary_chain = None
+        self._mining = False
+        self.parent = None
+        self.uncle = None
 
     # Graph
 
     def set_parent(self, parent):
         assert parent.hash == self.parent_hash
-        self.parent, = parent
+        self.parent = parent
 
     def set_uncle(self, uncle):
         if uncle == None:
@@ -115,13 +226,16 @@ class QuantaBlock(Encodium):
         assert uncle.hash == self.uncle_hash
         self.uncle = uncle
 
+    # Properties
+
     @property
     def primary_chain(self):
         if self._primary_chain == None:
-            self._primary_chain = [self] + self.parent.primary_chain
+            if self.parent_hash == 0:
+                self._primary_chain = [self]
+            else:
+                self._primary_chain = [self] + self.parent.primary_chain
         return self._primary_chain
-
-    # Properties
 
     @property
     def hash(self):
@@ -130,110 +244,16 @@ class QuantaBlock(Encodium):
     @property
     def sigmadiff(self):
         if self._sigmadiff == None:
-            self._sigmadiff = sum(map(target_to_diff, [i.target for i in order_from(root, self)]))
+            self._sigmadiff = sum(map(target_to_diff, [i.target for i in Graph.order_from(graph.root, self)]))
         return self._sigmadiff
 
+    @property
+    def coins_generated(self):
+        return DIFF_ONE // self.target - storage_fee(self)
 
-# Graph Functions
-
-def order_from(early_node, late_node, carry=[]):
-    if early_node == late_node: return [late_node]
-    if late_node.parent_hash == 0:
-        raise Exception('Root block encountered unexpectedly while ordering graph')
-    order = exclude_from(order_from(early_node, late_node.parent), carry)
-    if late_node.uncle is not None:
-        order += exclude_from(order_from(early_node, late_node.uncle), carry + order)
-    return order + [late_node]
-
-
-def exclude_from(a, b):
-    return [i for i in a if i not in b]
-
-
-def process_blocks(blocks):
-    global state
-    backup_state = deepcopy(state)
-    try:
-        for block in blocks:
-            _process_block(block)
-    except:
-        state = backup_state
-
-
-def _process_block(block):
-    if block.parent_hash not in block_index or (block.uncle is not None and block.uncle not in block_index):
-        return
-    if block.hash in block_index:
-        return
-    block.set_parent(block_index[block.parent_hash])
-    if block.uncle_hash is not None: block.set_uncle(block_index[block.uncle_hash])
-    if better_than_head(block):
-        reorganize(block)
-    else:
-        orphans.add(block)
-
-
-def better_than_head(block):
-    return block.sigmadiff > head.sigmadiff
-
-
-def reorganize(block):
-    pivot = find_pivot(head, block)
-    mass_unapply(order_from(pivot, head))
-    mass_apply(order_from(pivot, block))
-
-
-def find_pivot(b1, b2):
-    # conjecture: rewinding back to the lowest common parent in the primary chain (chain made of pri parents) is sufficient
-    for past_block in b1.primary_chain:
-        if past_block in b2.primary_chain:
-            return past_block
-    return None  # should probably not return None but do something else useful
-
-
-def mass_unapply(path):
-    for block in path[::-1]:
-        unapply_to_state(block)
-        orphans.add(block)
-
-
-def mass_apply(path):
-    for block in path:
-        assert valid_for_state(block)
-        apply_to_state(block)
-        orphans.remove(block)
-
-
-# State Functions
-
-def valid_for_state(block):
-    return state.get(block.tx.signature.pub_x) >= block.tx.value
-
-
-def apply_to_state(block):
-    modify_state(block, 1)
-
-
-def unapply_to_state(block):
-    modify_state(block, -1)
-
-
-def modify_state(block, direction):
-    assert direction in [-1, 1]
-    if block.tx is not None:
-        state.move_coins(block.tx.recipient, direction * block.tx.value)
-        state.move_coins(block.tx.signature.pub_x, -1 * direction * block.tx.value)
-    state.move_coins(block.coinbase, direction * coins_generated(block))
-
-
-# Monetary Functions
-
-def coins_generated(block):
-    return DIFF_ONE // block.target - storage_fee(block)
-
-
-def storage_fee(block):
-    return len(block.to_json()) * FEE_CONSTANT
+    @property
+    def acceptable_work(s):
+        return s.hash < s.target
 
 
 # Helpers
@@ -258,11 +278,20 @@ def target_to_diff(target):
     return DIFF_ONE // target
 
 
+def exclude_from(a, b):  # operates on paths
+    return [i for i in a if i not in b]
+
+
 # Crypto Helpers
 
 def valid_secp256k1_signature(x, y, msg, r, s):
     return ecdsa.verify(ecdsa.generator_secp256k1, (x, y), global_hash(msg), (r, s))
 
+
+# Network-y functions
+
+def storage_fee(block):
+    return len(block.to_json()) * FEE_CONSTANT
 
 
 # Messages
@@ -283,15 +312,7 @@ class BlockRequest(Encodium):
 
 @p2p.on_message(PUSHBLOCKS, BlockList)
 def respond_to_push(peer, block_list):
-    global state, head
-    old_head = head
-    backup_state = deepcopy(state)
-    try:
-        process_blocks(block_list.blocks)
-    except:
-        peer.disconnect()
-        state = backup_state
-        head = old_head
+    graph.add_blocks(block_list.blocks)
 
 
 @p2p.on_message(PULLBLOCKS, BlockRequest)
@@ -299,9 +320,11 @@ def respond_to_pull(peer, block_request):
     global block_index
     try:
         peer.send(PUSHBLOCKS, BlockList(
-            blocks=order_from(block_index[block_request.start_hash], block_index[block_request.end_hash])))
+            blocks=Graph.order_from(block_index[block_request.start_hash], block_index[block_request.end_hash])))
     except:
         peer.disconnect()
 
 
-root = QuantaBlock(parent_hash=0, target=(DIFF_ONE // (256 ** 3) - 1), coinbase=0, timestamp=0, nonce=1901667)  # this is the genesis block
+graph.set_root(QuantaBlock(parent_hash=0, target=(DIFF_ONE // (256 ** 3) - 1), coinbase=0, timestamp=0,
+                   nonce=1901667))  # this is the genesis block
+
